26cab09a4598b54658edda927ffffbfd2cd2572b
ZOUITA Nabil <zouita.nabil@ieee.org>
2019-03-27 17:50:56 +0100

    worked on second sheet function to be more generalize

--- App_BackEnd.py
+++ App_BackEnd.py
@@ -24,19 +24,29 @@ class ExcelPy:
         
     def ParseFSheet(self,PathExc):
         #self.temp = os.path.splitext(PathExc)
-        self.book = xlrd.open_workbook(PathExc)
-        self.sheet = self.book.sheet_by_index(0)
         self.list_Values = []
         self.list_EnVar = []
+
+        self.book = xlrd.open_workbook(PathExc)
+        self.sheet = self.book.sheet_by_index(0)
+        
         self.List_EnVar = self.sheet.col_values(0)
         self.List_Values = self.sheet.col_values(1)
+        
         return self.List_EnVar, self.List_Values 
 
-    def ParseSSheet(self,PathExc):
-        self.book = xlrd.open_workbook(PathExc)
-        self.sheet = self.book.sheet_by_index(1)
+    def ParseSSheet(self,PathExc,WSheetVal):
         self.LTest_EnVar = []
         self.LTest_Value = []
+        
+        self.book = xlrd.open_workbook(PathExc)
+        self.sheet = self.book.sheet_by_index(WSheetVal)
+        
+        self.LTest_EnVar = self.sheet.col_values(0)
+        self.LTest_Value = self.sheet.col_values(1)
+        
+        return self.LTest_Value, self.LTest_EnVar
+        '''
         for i in range(1,self.sheet.nrows):
             self.cell_value = self.sheet.cell(i,1).value
             self.cell_type = self.sheet.cell_type(i,1)
@@ -44,32 +54,40 @@ class ExcelPy:
                 self.LTest_EnVar.append(self.sheet.cell(i,1).value)
                 self.LTest_Value.append(int(self.sheet.cell(i,2).value))
             else:
-                break
+                break '''         
 
-        return self.LTest_Value, self.LTest_EnVar 
-
-    def LogEditorPass(self,PathExc):
+    def LogEditorPass(self,PathExc,WSheetVal):
         self.wb = openpyxl.load_workbook(PathExc)
-        self.sheet = self.wb.worksheets[0]
-        self.sheet.merge_cells('C1:D3')
+        
+        self.sheet = self.wb.worksheets[WSheetVal]
+        self.sheet.merge_cells('C1:D5')
+        
         self.cell = self.sheet.cell(row=1, column=3)
         self.cell.value = 'Test OK'
         self.cell.alignment = Alignment(horizontal='center', vertical='center')
 
         self.wb.save(PathExc)
 
-    def LogEditorNotPass(self,PathExc):
+    def LogEditorNotPass(self,PathExc,WSheetVal):
         self.wb = openpyxl.load_workbook(PathExc)
-        self.sheet = self.wb.worksheets[0]
-        self.sheet.merge_cells('E2:F6')
-        self.cell = self.sheet.cell(row=2, column=5)
+        
+        self.sheet = self.wb.worksheets[WSheetVal]
+        self.sheet.merge_cells('C1:D5')
+        
+        self.cell = self.sheet.cell(row=1, column=3)
         self.cell.value = 'Test Not OK'
         self.cell.alignment = Alignment(horizontal='center', vertical='center')
 
         self.wb.save(PathExc)
 
+
+class Py_CANoe:
+    def __init__(self):
+        pass
+    # Checking path will be done in Python_CANoe.py
     def Script_Editor(self,ConfigFile,EnvVar,EnvVal):
         self.file = open("Script.py","w")
+        
         self.file.write("from Python_CANoe import CANoe \n")
         self.file.write("var = CANoe() \n")
         self.file.write("var.open_simulation('%s') \n"%ConfigFile)
@@ -80,11 +98,6 @@ class ExcelPy:
         
         self.file.close()
 
-
-class Py_CANoe:
-    def __init__(self):
-        pass
-    # Checking path will be done in Python_CANoe.py
     def PyCan_Exec(self,path):
         self.root = CANoe()
         self.root.open_simulation(path) #CANoe function restriction are in package
