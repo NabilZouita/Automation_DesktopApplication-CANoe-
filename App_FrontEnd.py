c97cfd0f0f7f3ba9fcedd7ede98a46f90043c149
ZOUITA Nabil <zouita.nabil@ieee.org>
2019-04-12 12:22:58 +0100

    two major changes
    -New design is on the way (comparing QT Designer code generated and fixing orginal code)
    -Major changes to use Execute choose file option just for one time. (Two examples are working perfectly)
    -No need for a spinBox and it's method.
    -Index is being taking as it is.
    -Features to enhance :
    => Test 0 = Common Condition and should not be choosen.

--- App_FrontEnd.py
+++ App_FrontEnd.py
@@ -28,19 +28,20 @@ class Ui_MainWindow(QtGui.QWidget):
     def setupUi(self, MainWindow):
     
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(407, 300)
+        MainWindow.resize(532, 377)
         
         self.centralWidget = QtGui.QWidget(MainWindow)
         self.centralWidget.setObjectName(_fromUtf8("centralWidget"))
         
         self.frame = QtGui.QFrame(self.centralWidget)
-        self.frame.setGeometry(QtCore.QRect(20, 10, 421, 311))
+        
+        self.frame.setGeometry(QtCore.QRect(20, 10, 491, 311))
         self.frame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtGui.QFrame.Raised)
         self.frame.setObjectName(_fromUtf8("frame"))
         
         self.verticalLayoutWidget = QtGui.QWidget(self.frame)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 181, 131))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 261, 181))
         self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
         
         self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
@@ -49,19 +50,63 @@ class Ui_MainWindow(QtGui.QWidget):
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         
         self.widget = QtGui.QWidget(self.verticalLayoutWidget)
+        """ Block of code added of size policy """
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+
+        self.widget.setSizePolicy(sizePolicy)
+        """ It ends here """
+
         self.widget.setObjectName(_fromUtf8("widget"))
         
         self.pushButton = QtGui.QPushButton(self.widget)
-        self.pushButton.setGeometry(QtCore.QRect(10, 5, 81, 23))
+        
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 111, 61))
+        """ Block of code : PushButton Layout """
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Fixed)
+        
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        
+        self.pushButton.setSizePolicy(sizePolicy)
+        
+        font = QtGui.QFont()
+        
+        font.setFamily(_fromUtf8("Arial"))
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        
+        self.pushButton.setFont(font)
+        self.pushButton.setDefault(True)
+        """ End of Code """
+
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.pushButton.clicked.connect(self.load_sheet)
 
         self.pushButton_2 = QtGui.QPushButton(self.widget)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 30, 81, 23))
+        
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 110, 111, 61))
+        """ PushButton2 : Layout Design """
+        font = QtGui.QFont()
+        
+        font.setFamily(_fromUtf8("Arial"))
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setDefault(True)
+        """ End of Code """
+
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.pushButton_2.clicked.connect(self.load_config)
              
         self.pushButton_3 = QtGui.QPushButton(self.widget)
+        
         self.pushButton_3.setGeometry(QtCore.QRect(10, 55, 81, 23))
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.pushButton_3.setEnabled(False)
@@ -69,6 +114,7 @@ class Ui_MainWindow(QtGui.QWidget):
         self.pushButton_3.clicked.connect(self.generate_script)
         
         self.pushButton_4 = QtGui.QPushButton(self.widget)
+        
         self.pushButton_4.setGeometry(QtCore.QRect(10, 80, 81, 23))
         self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
         self.pushButton_4.setEnabled(False)
@@ -79,19 +125,23 @@ class Ui_MainWindow(QtGui.QWidget):
         ########### Design Text Edit ###########
 
         self.verticalLayoutWidget_2 = QtGui.QWidget(self.frame)
+        
         self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(190, 0, 181, 131))
         self.verticalLayoutWidget_2.setObjectName(_fromUtf8("verticalLayoutWidget_2"))
         
         self.verticalLayout_2 = QtGui.QVBoxLayout(self.verticalLayoutWidget_2)
+        
         self.verticalLayout_2.setMargin(11)
         self.verticalLayout_2.setSpacing(6)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
         
         self.horizontalLayoutWidget = QtGui.QWidget(self.frame)
+        
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 140, 371, 91))
         self.horizontalLayoutWidget.setObjectName(_fromUtf8("horizontalLayoutWidget"))
         
         self.horizontalLayout = QtGui.QHBoxLayout(self.horizontalLayoutWidget)
+        
         self.horizontalLayout.setMargin(11)
         self.horizontalLayout.setSpacing(6)
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
@@ -99,6 +149,7 @@ class Ui_MainWindow(QtGui.QWidget):
         self.widget_3 = QtGui.QWidget(self.horizontalLayoutWidget)
         
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.widget_3.sizePolicy().hasHeightForWidth())
@@ -107,6 +158,7 @@ class Ui_MainWindow(QtGui.QWidget):
         self.widget_3.setObjectName(_fromUtf8("widget_3"))
         
         self.textEdit = QtGui.QTextEdit(self.widget_3)
+        
         self.textEdit.setGeometry(QtCore.QRect(0, 0, 337, 59))
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
         #self.textEdit.clicked.connect(self.TextEditor)
@@ -116,15 +168,18 @@ class Ui_MainWindow(QtGui.QWidget):
         ############ RadioButtons Layout Code ###########
 
         self.widget_2 = QtGui.QWidget(self.frame)
+        
         self.widget_2.setGeometry(QtCore.QRect(190, 0, 179, 71))
         self.widget_2.setObjectName(_fromUtf8("widget_2"))
         
         self.radioButton = QtGui.QRadioButton(self.widget_2)
+        
         self.radioButton.setGeometry(QtCore.QRect(20, 20, 82, 17))
         self.radioButton.setObjectName(_fromUtf8("radioButton"))
         self.radioButton.clicked.connect(self.test_passed)
         
         self.radioButton_2 = QtGui.QRadioButton(self.widget_2)
+        
         self.radioButton_2.setGeometry(QtCore.QRect(20, 50, 82, 17))
         self.radioButton_2.setObjectName(_fromUtf8("radioButton_2"))
         self.radioButton_2.clicked.connect(self.test_notPassed)
@@ -132,20 +187,24 @@ class Ui_MainWindow(QtGui.QWidget):
         ############ The End ##############
 
         self.verticalLayoutWidget_3 = QtGui.QWidget(self.frame)
+        
         self.verticalLayoutWidget_3.setGeometry(QtCore.QRect(189, 80, 181, 61))
         self.verticalLayoutWidget_3.setObjectName(_fromUtf8("verticalLayoutWidget_3"))
         
         self.verticalLayout_3 = QtGui.QVBoxLayout(self.verticalLayoutWidget_3)
+        
         self.verticalLayout_3.setMargin(11)
         self.verticalLayout_3.setSpacing(6)
         self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
         
         self.widget_4 = QtGui.QWidget(self.verticalLayoutWidget_3)
+        
         self.widget_4.setObjectName(_fromUtf8("widget_4"))
 
         ########## ComboBox Layout Code ###########
         
         self.comboBox = QtGui.QComboBox(self.widget_4)
+        
         self.comboBox.setGeometry(QtCore.QRect(10, 0.75, 91, 22))
         self.comboBox.setObjectName(_fromUtf8("comboBox"))
         self.comboBox.currentIndexChanged.connect(self.SecondSheet)  
@@ -189,6 +248,7 @@ class Ui_MainWindow(QtGui.QWidget):
         ######## The End ############## 
 
         self.spinBox = QtGui.QSpinBox(self.widget_4)
+        
         self.spinBox.setGeometry(QtCore.QRect(110, 0.75, 42, 22))
         self.spinBox.setObjectName(_fromUtf8("spinBox"))
         self.spinBox.setMinimum(0)
@@ -200,48 +260,63 @@ class Ui_MainWindow(QtGui.QWidget):
         MainWindow.setCentralWidget(self.centralWidget)
         
         self.menuBar = QtGui.QMenuBar(MainWindow)
+        
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 407, 21))
         self.menuBar.setObjectName(_fromUtf8("menuBar"))
         
         self.menuFile = QtGui.QMenu(self.menuBar)
+        
         self.menuFile.setObjectName(_fromUtf8("menuFile"))
         
         self.menuHelp = QtGui.QMenu(self.menuBar)
+        
         self.menuHelp.setObjectName(_fromUtf8("menuHelp"))
+        
         MainWindow.setMenuBar(self.menuBar)
         
         self.mainToolBar = QtGui.QToolBar(MainWindow)
+        
         self.mainToolBar.setObjectName(_fromUtf8("mainToolBar"))
+        
         MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
         
         self.statusBar = QtGui.QStatusBar(MainWindow)
+        
         self.statusBar.setObjectName(_fromUtf8("statusBar"))
+        
         MainWindow.setStatusBar(self.statusBar)
         
         self.actionOpen_Sheet = QtGui.QAction(MainWindow)
+        
         self.actionOpen_Sheet.setObjectName(_fromUtf8("actionOpen_Sheet"))
         
         self.actionOpen_Config = QtGui.QAction(MainWindow)
+        
         self.actionOpen_Config.setObjectName(_fromUtf8("actionOpen_Config"))
         
         self.actionOpen_Script = QtGui.QAction(MainWindow)
+        
         self.actionOpen_Script.setObjectName(_fromUtf8("actionOpen_Script"))
         
         self.actionSave_As = QtGui.QAction(MainWindow)
+        
         self.actionSave_As.setObjectName(_fromUtf8("actionSave_As"))
         
         self.actionQuit = QtGui.QAction(MainWindow)
+        
         self.actionQuit.setObjectName(_fromUtf8("actionQuit"))
         self.actionQuit.setShortcut("Ctrl+Q")
         self.actionQuit.setStatusTip("Close the Application !")
         self.actionQuit.triggered.connect(self.close_application)
         
         self.actionAbout_this_App = QtGui.QAction(MainWindow)
+        
         self.actionAbout_this_App.setObjectName(_fromUtf8("actionAbout_this_App"))
         self.actionAbout_this_App.setStatusTip("App Summary !")
         self.actionAbout_this_App.triggered.connect(self.app_summary)
         
         self.actionAbout = QtGui.QAction(MainWindow)
+        
         self.actionAbout.setObjectName(_fromUtf8("actionAbout"))
         self.actionAbout.setStatusTip("App documentation !")
         self.actionAbout.triggered.connect(self.app_documentation)
@@ -249,10 +324,12 @@ class Ui_MainWindow(QtGui.QWidget):
         self.menuFile.addAction(self.actionOpen_Sheet)
         self.menuFile.addAction(self.actionOpen_Config)
         self.menuFile.addAction(self.actionOpen_Script)
+        
         self.menuFile.addSeparator()
         
         self.menuFile.addAction(self.actionSave_As)
         self.menuFile.addAction(self.actionQuit)
+        
         self.menuHelp.addAction(self.actionAbout_this_App)
         self.menuHelp.addAction(self.actionAbout)
         
@@ -260,18 +337,23 @@ class Ui_MainWindow(QtGui.QWidget):
         self.menuBar.addAction(self.menuHelp.menuAction())
 
         self.retranslateUi(MainWindow)
+        
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
     def load_sheet(self):
         try:
             self.ExcelSheet = QtGui.QFileDialog.getOpenFileName(self,'Single File','*.xlsx')
+            
             self.InstVar = App_BackEnd.ExcelPy()
             self.EnvVar = self.InstVar.ParseFSheet(str(self.ExcelSheet))[0]
             self.ValVar = self.InstVar.ParseFSheet(str(self.ExcelSheet))[1]
+            
             #print self.EnvVar
             #print self.ValVar
+            
             self.message = ("Loaded successfully: %s \n"%os.path.split(str(self.ExcelSheet))[1])
+            
             self.textEdit.clear()
             self.textEdit.setTextColor(QtGui.QColor("green"))
             self.textEdit.insertPlainText(self.message)
@@ -279,6 +361,7 @@ class Ui_MainWindow(QtGui.QWidget):
             callVar = self.ValueChange()
         except IOError:
             self.message = "You did not choose a file \n Please check again!"
+            
             self.textEdit.clear()
             self.textEdit.setTextColor(QtGui.QColor("red"))
             self.textEdit.insertPlainText(self.message)
@@ -297,17 +380,22 @@ class Ui_MainWindow(QtGui.QWidget):
 
     def load_config(self):
         self.ConfigFile = QtGui.QFileDialog.getOpenFileName(self,'Single File','*.cfg')
+        
         self.FileName = os.path.split(str(self.ConfigFile))[1]
+        
         if (self.FileName != ""):
             #print self.ConfigFile
             self.message = ("Loaded successfully: %s"%os.path.split(str(self.ConfigFile))[1])
+            
             self.textEdit.clear()
             self.textEdit.setTextColor(QtGui.QColor("green"))
             self.textEdit.insertPlainText(self.message)
+            
             self.pushButton_3.setEnabled(True)
             self.pushButton_4.setEnabled(True)
         else:
             self.message = ("You did not choose a correct Config File \n Please Check !")
+            
             self.textEdit.clear()
             self.textEdit.setTextColor(QtGui.QColor("red"))
             self.textEdit.insertPlainText(self.message)
@@ -415,16 +503,17 @@ class Ui_MainWindow(QtGui.QWidget):
         #Previous Value before change is "self.spinBox.value()"
         for i in range(0,TempRes):
             self.comboBox.addItem(_fromUtf8(""))
-            self.comboBox.setItemText(i, _translate("MainWindow", "Test %d"%(i+1), None))
+            self.comboBox.setItemText(i, _translate("MainWindow", "Test %d"%i, None))
 
 
     def SecondSheet(self,index):
-        #self.keys = True
-        self.varTemp = index + 1
-        print ("Current index:",((index)+1))
+        self.varTemp = index
+        print ("Current index:",index)
+        
         self.InstVar = App_BackEnd.ExcelPy()
-        self.envVar = self.InstVar.ParseSSheet((str(self.ExcelSheet)),((index)+1))[0]
-        self.valVar = self.InstVar.ParseSSheet((str(self.ExcelSheet)),((index)+1))[1]
+        
+        self.envVar = self.InstVar.ParseSSheet((str(self.ExcelSheet)),(index))[0]
+        self.valVar = self.InstVar.ParseSSheet((str(self.ExcelSheet)),(index))[1]
         
         print self.envVar
         print self.valVar
@@ -446,6 +535,7 @@ class Ui_MainWindow(QtGui.QWidget):
 
     def app_summary(self):
         message_box = QtGui.QMessageBox()
+        
         message_box.setIcon(message_box.Information)
         message_box.setWindowTitle("About this Application")
         message_box.setWindowIcon(QtGui.QIcon('python_logo.png'))
@@ -500,11 +590,17 @@ class Ui_MainWindow(QtGui.QWidget):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        
         self.pushButton.setText(_translate("MainWindow", "Load", None))
+        
         self.pushButton_2.setText(_translate("MainWindow", "Configuration", None))
+        
         self.pushButton_3.setText(_translate("MainWindow", "Generate", None))
+        
         self.pushButton_4.setText(_translate("MainWindow", "Execute", None))
+        
         self.radioButton.setText(_translate("MainWindow","Test OK",None))
+        
         self.radioButton_2.setText(_translate("MainWindow","Test Not OK",None))
         """
         self.comboBox.setItemText(0, _translate("MainWindow", "Test 1", None))
@@ -544,23 +640,36 @@ class Ui_MainWindow(QtGui.QWidget):
         self.comboBox.setItemText(34, _translate("MainWindow", "Test 35", None))
         """
         self.menuFile.setTitle(_translate("MainWindow", "File", None))
+        
         self.menuHelp.setTitle(_translate("MainWindow", "Help", None))
+        
         self.actionOpen_Sheet.setText(_translate("MainWindow", "Open Sheet", None))
+        
         self.actionOpen_Config.setText(_translate("MainWindow", "Open Config", None))
+        
         self.actionOpen_Script.setText(_translate("MainWindow", "Open Script", None))
+        
         self.actionSave_As.setText(_translate("MainWindow", "Save As", None))
+        
         self.actionQuit.setText(_translate("MainWindow", "Quit", None))
+        
         self.actionAbout_this_App.setText(_translate("MainWindow", "About this App", None))
+        
         self.actionAbout.setText(_translate("MainWindow", "Documentation", None))
 
 
 if __name__ == "__main__":
     import sys
+    
     app = QtGui.QApplication(sys.argv)
+    
     MainWindow = QtGui.QMainWindow()
+    
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    
     MainWindow.setWindowIcon(QtGui.QIcon('python_logo.png'))
     MainWindow.setWindowTitle("PFE2019_AltranTunisie_IHM")
     MainWindow.show()
+    
     sys.exit(app.exec_())
